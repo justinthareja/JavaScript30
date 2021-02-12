d02223b4e581f1b8fc313ae4aeaf43fcec1eb72f
Justin Thareja <justin.thareja@gmail.com>
2021-02-11 21:11:37 -0500

    updated drum kit to use transitionend event

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,51 +1,26 @@
-(function () {
-    document.body.addEventListener("keydown", handleKeyDown);
-    document.body.addEventListener("keyup", handleKeyUp);
+const $keys = document.querySelector(".keys");
+
+$keys.addEventListener("transitionend", handleTransitionEnd);
+
+window.addEventListener("keydown", handleKeyDown);
+
+function handleTransitionEnd(e) {
+    if (!e.target.matches(".key")) return;
+    if (e.propertyName !== "transform") return;
     
-    function handleKeyDown(e) {
-        toggleStyle(e.keyCode);
-        play(e.keyCode);
-    }
-    
-    function handleKeyUp(e) {
-        toggleStyle(e.keyCode);
-    }
-    
-    function toggleStyle(id) {
-        var $key = getKey(id);
-        
-        if (!$key) {
-            return;
-        }
-            
-        $key.classList.toggle("playing");
-    }
-    
-    
-    function getKey(id) {
-        return document.querySelector(`.key[data-key="${id}"]`);
-    }
-    
-    function getAudio(id) {
-        return document.querySelector(`audio[data-key="${id}"]`);
-    }
-    
-    
-    async function play(id) {
-        var $audio = getAudio(id);
-    
-        if (!$audio) {
-            return false;
-        }
-        
-        try {
-            $audio.currentTime = 0;
-            await $audio.play();
-        } catch(err) {
-            
-        }
-    }
-})();
+    e.target.classList.remove("playing");
+}
+
+function handleKeyDown(e) {
+    const $audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const $key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!$audio) return;
+
+    $audio.currentTime = 0;
+    $audio.play();
+    $key.classList.add("playing");
+}
 
 
 
